d3e2a805c27b5c5fae6bd240e75b164488e6359e
“lucifer2355” <dgajjar999@gmail.com>
2021-01-19 22:24:18 +0530

    add news listing screen

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 
 import HomeScreen from './src/screens/HomeScreen';
+import NewsListingScreen from './src/screens/NewsListingScreen';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <NewsListingScreen />
     </>
   );
 };
---
+++ src/screens/NewsListingScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const NewsListingScreen = () => {
+  return (
+    <View>
+      <Text>Mews Listing SCren</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default NewsListingScreen;
